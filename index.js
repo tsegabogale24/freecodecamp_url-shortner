2e5e4a68706e0f5e7c7fb4f7bdab483f3838f5b6
tsega b <tsegabogale92@gmail.com>
2026-02-09 16:33:56 +0300

    add url shortner through in memory db

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const dns = require('dns');
 const cors = require('cors');
 const app = express();
-
+const bodyParser = require('body-parser');
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -18,7 +20,32 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
-
+urldatabase = [];
+app.post('/api/shorturl' , (req , res) => {
+  original_url = req.body.url;
+  const urlObj = new URL(original_url);
+ const hostname = urlObj.hostname;
+  dns.lookup(hostname , (err , address , family) => {
+    if(err){
+      res.json({error: "invalid url"})
+    }
+    else{
+      shortUrl = urldatabase.length + 1;
+      urldatabase.push({original_url: original_url , short_url: shortUrl});
+      res.json({original_url: original_url , short_url: shortUrl});
+    }
+  })
+})
+app.get('/api/:shorturl' , (req , res) => {
+  shorturl = req.params.shorturl;
+  url = urldatabase.find(url => url.short_url == shorturl);
+  if(url){
+    res.redirect(url.original_url);
+  }
+  else{
+    res.json({error: "short url not found"});
+  }
+})
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
