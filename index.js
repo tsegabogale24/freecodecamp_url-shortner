bf785f87869754eed1ac2ebe401000a3c65b7f70
tsega b <tsegabogale92@gmail.com>
2026-02-09 16:51:01 +0300

    modify api arrangement

--- index.js
+++ index.js
@@ -21,21 +21,32 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 urldatabase = [];
-app.post('/api/shorturl' , (req , res) => {
-  original_url = req.body.url;
-  const urlObj = new URL(original_url);
- const hostname = urlObj.hostname;
-  dns.lookup(hostname , (err , address , family) => {
-    if(err){
-      res.json({error: "invalid url"})
-    }
-    else{
-      shortUrl = urldatabase.length + 1;
-      urldatabase.push({original_url: original_url , short_url: shortUrl});
-      res.json({original_url: original_url , short_url: shortUrl});
-    }
-  })
-})
+app.post('/api/shorturl', (req, res) => {
+  let original_url = req.body.url;
+
+  // If the URL doesn’t start with http:// or https://, prepend http://
+  if (!/^https?:\/\//i.test(original_url)) {
+    original_url = 'http://' + original_url;
+  }
+
+  // Validate URL
+  let urlObj;
+  try {
+    urlObj = new URL(original_url);
+  } catch {
+    return res.json({ error: "invalid url" });
+  }
+
+  dns.lookup(urlObj.hostname, (err) => {
+    if (err) return res.json({ error: "invalid url" });
+
+    const short_url = urldatabase.length + 1;
+    urldatabase.push({ original_url, short_url });
+
+    res.json({ original_url, short_url });
+  });
+});
+
 app.get('/api/shorturl/:shorturl', (req, res) => {
   const shorturl = req.params.shorturl;
   const url = urldatabase.find(u => u.short_url == shorturl);
